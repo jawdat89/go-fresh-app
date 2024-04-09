1c35df8d8c0a526ea9bb2fb505af2029deee115f
jawdat89 <jawdat.89@gmail.com>
2024-04-09 12:05:44 +0300

    tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "primary-lighter": "#F77C50",
+        primary: "#ED5226",
+        "primary-darker": "#C4461E",
+        "secondary-lighter": "#A4D065",
+        secondary: "#90C843",
+        "secondary-darker": "#78A82B",
+        white: "#F4F4F4",
+        "gray-100": "#E5E5E5",
+        "gray-200": "#CCCCCC",
+        "gray-300": "#B3B3B3",
+      },
+    },
+  },
+  plugins: [],
+};
