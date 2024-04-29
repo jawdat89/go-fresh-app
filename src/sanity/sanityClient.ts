62ae5f62e390230aee398418bffd197e099a53a2
Jawdat <jawdat.89@gmail.com>
2024-04-29 23:35:40 +0300

    vercel sanity key valut fix

--- src/sanity/sanityClient.ts
+++ src/sanity/sanityClient.ts
@@ -1,11 +1,23 @@
 import { createClient } from "@sanity/client";
 
+// Dynamically select the API token based on the environment
+const getApiToken = () => {
+  switch (import.meta.env.MODE) {
+    case "production":
+      return import.meta.env.VITE_VERCEL_SANITY_TOKEN;
+    case "development":
+      return import.meta.env.VITE_SANITY_TOKEN;
+    default:
+      return import.meta.env.VITE_SANITY_TOKEN;
+  }
+};
+
 const config = {
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID!,
-  dataset: import.meta.env.VITE_SANITY_DATASET!,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
   useCdn: true, // `false` if you want fresh data
   apiVersion: "2024-04-09", // use a specific API version
-  token: import.meta.env.VITE_SANITY_API_TOKEN!,
+  token: getApiToken(), // Use the function to get the token based on environment
 };
 
 export const sanityClient = createClient(config);
