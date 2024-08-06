95cbac00ddb3a5e5a5b5f769bab7039a5435da94
jawdat-dev <35340081+jawdat89@users.noreply.github.com>
2024-08-06 12:58:46 +0300

    encrypt data

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Determine if we are in development mode
+const isDevelopment = process.env.NODE_ENV === "development";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -13,4 +16,21 @@ export default defineConfig({
       "@/pages": "/src/pages",
     },
   },
+  build: {
+    outDir: "dist",
+    sourcemap: isDevelopment, // Disable source maps in production for security
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+      plugins: [
+        // visualizer({
+        //   filename: "dist/stats.html", // You can specify the output file for the visualization
+        //   open: true, // Open the file automatically in the browser
+        // }),
+      ],
+    }
+  }
 });
